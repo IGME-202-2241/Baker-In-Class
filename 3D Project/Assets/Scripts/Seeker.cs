9ebbef586ab5f8a722450495495c5fa74b5a050f
eabigm <eabigm@GOL2570-00>
2024-11-14 11:58:26 -0500

    Week 12 day 2, bounds stuff

--- 3D Project/Assets/Scripts/Seeker.cs
+++ 3D Project/Assets/Scripts/Seeker.cs
@@ -1,13 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Seeker : Agent
 {
     public GameObject target;
 
+    Vector3 seekForce;
+    public float seekScalar;
+
+    public Vector3 worldBounds;
+    Vector3 boundsForce;
+    public float boundsScalar = 1f;
+
     protected override Vector3 CalcSteering()
     {
-        return Seek(target);
+        seekForce = Seek(target) * seekScalar;
+
+        //  Check for in of bounds
+        // if yes seek(vetor.zero)
+        if (transform.position.x < worldBounds.x / 2f &&
+            transform.position.x > worldBounds.x / -2f &&
+            transform.position.z < worldBounds.z / 2f &&
+            transform.position.z > worldBounds.z / -2f)
+        {
+            boundsForce = Vector3.zero;
+        }
+        else
+        {
+            boundsForce = Seek(Vector3.zero);
+
+        }
+
+        return seekForce + boundsForce;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, velocity);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, seekForce);
+
+        //
+        //  Bounds
+        //
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(Vector3.zero, worldBounds);
+
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawRay(transform.position, boundsForce);
     }
 }
